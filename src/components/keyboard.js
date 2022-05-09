91e86b27486f730654ef2d598bbce7d5cc3b56e1
JiRad71 <kirilllkorolev@gmail.com>
2022-05-09 17:47:55 +0300

    feat: add render button, and inpot for textarea

--- src/components/keyboard.js
+++ src/components/keyboard.js
@@ -1,8 +1,7 @@
 import Control from "../utils/control";
 import Button from "./button";
-import Row from "./rows";
 
-const btn = [
+ export const btn = [
     [
       { classname: "keyboard-button", val: "`", enUp: "~", ru: "ё", ruUp: "Ё", code: "Backquote" },
       { classname: "keyboard-button", val: "1", enUp: "!", ru: "1", ruUp: "!", code: "Digit1" },
@@ -17,10 +16,10 @@ const btn = [
       { classname: "keyboard-button", val: "0", enUp: ")", ru: "0", ruUp: ")", code: "Digit0" },
       { classname: "keyboard-button", val: "-", enUp: "_", ru: "-", ruUp: "_", code: "Minus" },
       { classname: "keyboard-button", val: "=", enUp: "+", ru: "=", ruUp: "+", code: "Equal" },
-      { classname: "keyboard-button keyboard-long", val: "Backspace", code: "Backspace" }
+      { classname: "keyboard-button keyboard-long", val: "Backspace", ru: "Backspace", code: "Backspace", type: "functional" }
     ],
     [
-      { classname: "keyboard-button keyboard-long", val: "Tab", code: "Tab" },
+      { classname: "keyboard-button keyboard-long", val: "Tab", ru: "Tab", code: "Tab", type: "functional" },
       { classname: "keyboard-button", val: "q", enUp: "Q", ru: "й", ruUp: "Й", code: "KeyQ" },
       { classname: "keyboard-button", val: "w", enUp: "W", ru: "ц", ruUp: "Ц", code: "KeyW" },
       { classname: "keyboard-button", val: "e", enUp: "E", ru: "у", ruUp: "У", code: "KeyE" },
@@ -33,11 +32,11 @@ const btn = [
       { classname: "keyboard-button", val: "p", enUp: "P", ru: "з", ruUp: "З", code: "KeyP" },
       { classname: "keyboard-button", val: "[", enUp: "{", ru: "х", ruUp: "Х", code: "BracketLeft" },
       { classname: "keyboard-button", val: "]", enUp: "}", ru: "ъ", ruUp: "Ъ", code: "BracketRight" },
-      { classname: "keyboard-button", val: "\\",enUp: "|","r": "\\", ruUp: "/", code: "Backslash" },
-      { classname: "keyboard-button keyboard-long", val: "Del", code: "Delete" }
+      { classname: "keyboard-button", val: "\\",enUp: "|", ru: "\\", ruUp: "/", code: "Backslash" },
+      { classname: "keyboard-button keyboard-long", val: "Del", ru: "Del", code: "Delete", type: "functional" }
     ],
     [
-      { classname: "keyboard-button keyboard-long", val: "CapsLock", code: "CapsLock" },
+      { classname: "keyboard-button keyboard-long", val: "CapsLock", ru: "CapsLock", code: "CapsLock", type: "functional" },
       { classname: "keyboard-button", val: "a", enUp: "A", ru: "ф", ruUp: "Ф", code: "KeyA" },
       { classname: "keyboard-button", val: "s", enUp: "S", ru: "ы", ruUp: "Ы", code: "KeyS" },
       { classname: "keyboard-button", val: "d", enUp: "D", ru: "в", ruUp: "В", code: "KeyD" },
@@ -48,11 +47,11 @@ const btn = [
       { classname: "keyboard-button", val: "k", enUp: "K", ru: "л", ruUp: "Л", code: "KeyK" },
       { classname: "keyboard-button", val: "l", enUp: "L", ru: "д", ruUp: "Д", code: "KeyL" },
       { classname: "keyboard-button", val: ";", enUp: ":", ru: "ж", ruUp: "Ж", code: "Semicolon" },
-      { classname: "keyboard-button", val: "'", enUp: "\"","r": "э",ruUp: "Э", code: "Quote" },
-      { classname: "keyboard-button keyboard-long", val: "Enter", code: "Enter" }
+      { classname: "keyboard-button", val: "'", enUp: "\"",ru: "э",ruUp: "Э", code: "Quote" },
+      { classname: "keyboard-button keyboard-long", val: "Enter", ru: "Enter", code: "Enter" , type: "functional"}
     ],
     [
-      { classname: "keyboard-button keyboard-long", val: "Shift", code: "ShiftLeft" },
+      { classname: "keyboard-button keyboard-long", val: "Shift", ru:"Shift", code: "ShiftLeft", type: "functional" },
       { classname: "keyboard-button", val: "z", enUp: "Z", ru: "я", ruUp: "Я", code: "KeyZ" },
       { classname: "keyboard-button", val: "x", enUp: "X", ru: "ч", ruUp: "Ч", code: "KeyX" },
       { classname: "keyboard-button", val: "c", enUp: "C", ru: "с", ruUp: "С", code: "KeyC" },
@@ -63,30 +62,43 @@ const btn = [
       { classname: "keyboard-button", val: ",", enUp: "<", ru: "б", ruUp: "Б", code: "Comma" },
       { classname: "keyboard-button", val: ".", enUp: ">", ru: "ю", ruUp: "Ю", code: "Period" },
       { classname: "keyboard-button", val: "/", enUp: "?", ru: ".", ruUp: ",", code: "Slash" },
-      { classname: "keyboard-button", val: "↑", code: "ArrowUp" },
-      { classname: "keyboard-button keyboard-long-fixed", val: "Shift", code: "ShiftRight" }
+      { classname: "keyboard-button", val: "↑", ru: "↑", code: "ArrowUp" },
+      { classname: "keyboard-button keyboard-long-fixed", val: "Shift", ru: "Shift", code: "ShiftRight" , type: "functional"}
     ],
     [
-      { classname: "keyboard-button", val: "Ctrl", code: "ControlLeft" },
-      { classname: "keyboard-button", val: "Win", code: "MetaLeft" },
-      { classname: "keyboard-button", val: "Alt", code: "AltLeft" },
+      { classname: "keyboard-button", val: "Ctrl", ru: "Ctrl", code: "ControlLeft", type: "functional" },
+      { classname: "keyboard-button", val: "Win",ru: "Win", code: "MetaLeft" , type: "functional"},
+      { classname: "keyboard-button", val: "Alt",ru: "Alt", code: "AltLeft", type: "functional" },
       { classname: "keyboard-button keyboard-long",   val: " ", enUp: " ", ru: " ", ruUp: " ", code: "Space" },
-      { classname: "keyboard-button", val: "Alt", code: "AltRight" },
-      { classname: "keyboard-button", val: "←", code: "ArrowLeft" },
-      { classname: "keyboard-button", val: "↓", code: "ArrowDown" },
-      { classname: "keyboard-button", val: "→", code: "ArrowRight" },
-      { classname: "keyboard-button", val: "Ctrl", code: "ControlRight" }
+      { classname: "keyboard-button", val: "Alt", ru: "Alt", code: "AltRight", type: "functional" },
+      { classname: "keyboard-button", val: "←",ru: "←", code: "ArrowLeft", type: "functional" },
+      { classname: "keyboard-button", val: "↓",ru: "↓", code: "ArrowDown", type: "functional" },
+      { classname: "keyboard-button", val: "→",ru: "→", code: "ArrowRight", type: "functional" },
+      { classname: "keyboard-button", val: "Ctrl", ru: "Ctrl", code: "ControlRight" , type: "functional"}
     ]
   ]
   
 class Keyboard extends Control {
     constructor(parentNode){
         super(parentNode, 'div', 'keyboard-area')
-       btn.forEach((row)=>{
-            const r = new Row (this.node, row);
-        })
+        this.textArea = new Control (this.node, 'textarea', 'textarea');
+        }
+        setInputTextarea(val){
+          this.textArea.node.textContent+=val;
+        }
+        renderButtons(data, lang){
+          console.log(data) 
+          data.forEach((_,i)=>{
+            const row = new Control (this.node, 'div', 'keyboard-row');
+            data[i].forEach((btn)=>{
+             const button = new Button(row.node, btn.classname, btn[lang]);
+             button.node.onclick= ()=>{
+               if(!btn.type) this.setInputTextarea(btn[lang]);
+             }
+            })
+          })
+        }
     }
-}
 
 
-export default Keyboard
+export default Keyboard;
